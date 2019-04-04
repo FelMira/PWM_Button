2fa6931b434b8c22833d8a65fd1f90aaa8031087
Mira <felipemira12@hotmail.com>
2019-04-04 10:59:16 -0300

    Intensity and direction fully done

--- src/dimmer.cpp
+++ src/dimmer.cpp
@@ -1,9 +1,9 @@
 #include "dimmer.hpp"
 
-PwmOut Leds[]={(PD_13),(PD_15),(PD_9)};
+PwmOut Leds[]={(PD_13),(PD_15),(PA_9)};
 Timeout contador;
 InterruptIn botao(PA_0);
-bool direcao=0;
+bool direcao=0;//0=DOWN; 1=UP
 
 void ativar_timer(){
     contador.attach(&mudar_intensidade,1);
@@ -12,16 +12,38 @@ void ativar_timer(){
 
 void mudar_sentido(){
     contador.detach();
-    direcao = !direcao; //1=mais brilho; 0=menos brilho
+    Leds[direcao] = 0;
+    direcao = !direcao;
+    Leds[direcao] = 1; //1=mais brilho; 0=menos brilho
 }
 
 void mudar_intensidade(){
-    if(direcao==1){
-        
-        //Aumentar 20% do valor max. a cada segundo
+    botao.fall(NULL);
+    if(direcao==1){//Aumentar 5% do valor max. a cada segundo
+        if(Leds[2]<1.0){
+            Leds[2] = Leds[2]+0.05; 
         }
-    else{
-        //Diminuir na mesma passada que acima
+        if(Leds[2]>=1.0){
+            blinky_blue();
         }
+    }
+    if(direcao==0){//Diminuir na mesma passada que acima
+        if(Leds[2]>0.0){
+            Leds[2] = Leds[2]-0.05; 
+        }
+        if(Leds[2]<=0.0){
+            blinky_orange();
+        }
+    }
     contador.attach(&mudar_intensidade, 1);
+}
+
+void blinky_blue(){
+    Leds[1] = !Leds[1];
+    contador.attach(&blinky_blue, 0.1);
+}
+
+void blinky_orange(){
+    Leds[0] = !Leds[0];
+    contador.attach(&blinky_orange, 0.1);
 }
--- src/dimmer.hpp
+++ src/dimmer.hpp
@@ -2,10 +2,12 @@
 #define DIMMER_HPP
 #include "mbed.h"
 
-extern DigitalOut Leds[];
+extern PwmOut Leds[];
 extern InterruptIn botao;
 void ativar_timer();
 void mudar_sentido();
 void mudar_intensidade();
+void blinky_blue();
+void blinky_orange();
 
 #endif
